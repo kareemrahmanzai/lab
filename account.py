67f27f3f5e31bbafa0d908803f48d5ece74fe890
kareemrahmanzai <111786444+kareemrahmanzai@users.noreply.github.com>
2023-04-20 12:56:28 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,41 +1,45 @@
 class account:
-     """ This is an account class to compute several bank tasks
-     """
-     
-    def __init__ (self, name: str) -> None:
+    """ This is an account class to compute several bank tasks
+    """
+
+
+    def __init__(self, name: str) -> None:
         """ Sets object of the class
         """
-        self.__account_name = name  
+        self.__account_name = name
         self.__account_balance = 0
 
-    
+
     def deposit(self, amount):
         """ Adds amount to account balance or returns False
         """
-        if amount > 0:  
-            self.__account_balance = self.__account_balance + amount 
+        if amount > 0:
+            self.__account_balance = self.__account_balance + amount
             return True
-        else:   
+        else:
             return False
-            
+
+
     def withdraw(self, amount):
         """ Subtracts amount from account balance or returns False
         """
-        if amount > 0: 
-            if amount <= self.__account_balance: 
-                self.__account_balance = self.__account_balance-amount  
+        if amount > 0:
+            if amount <= self.__account_balance:
+                self.__account_balance = self.__account_balance - amount
                 return True
-            else:    
+            else:
                 return False
-        else:   
+        else:
             return False
-        
+
+
     def getbalance(self) -> str:
         """ use methods to return account balance via account_balance private variable
         """
-        return self.__account_balance 
-        
+        return self.__account_balance
+
+
     def getname(self) -> str:
         """ returns account name via account_name private variable
         """
-        return self.__account_name   
+        return self.__account_name
